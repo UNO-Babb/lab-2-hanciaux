bd0e84df296186ae5e591c4d4cf9447a5b22859d
hanciaux <hanciaux@unomaha.edu>
2025-01-29 15:50:52 +0000

    Finished future time

--- FutureTime.py
+++ FutureTime.py
@@ -9,21 +9,28 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 6)%24
   currentMinute = now.minute
 
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
+  hours = input("How many hours? ")
   #Ask user for minutes
-
+  minutes = input("How many minutes? ")
   #Calculate the time after the user-supplied time has passed.
-
+  hours=int(hours)
+  minutes=int(minutes)
+  newHour=(currentHour + hours)
+  newMinute=(currentMinute + minutes)
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
-
-
+  newHour = (newHour%12)
+  newMinute = (newMinute%60)
+  newHour = str(newHour)
+  newMinute = str(newMinute)
+  print(newHour + ":" + newMinute)
 if __name__ == '__main__':
   main()
