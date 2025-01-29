1075557fba12ebe0c4dfa87e243928db074bd9f1
hanciaux <hanciaux@unomaha.edu>
2025-01-29 15:29:13 +0000

    Finished 8 Ball

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,8 +10,19 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+answers=["Certainly", "No.", "Couldn't tell you", "I don't know", "Perchance", "Perhaps", "Tossup", "I have zero clue", "Don't count on it", 
+         "Why would you ask such a question?", "Without a shadow of a doubt", "My independent sources say no", "I don't know, so quit asking",
+         "Could happen", "Ask again later", "Ask someone else"]
   #Answer question randomly with one of the options from your earlier list.
+num=random.random() #decimal 0-1
+num=num*1000 #number 0-1000
+num=int(num) #no decimals
+num=num%16 #0-16
+
+question=input("Ask a yes or no question: ")
+print(answers[num]) #
+
+
 
 
 if __name__ == '__main__':
